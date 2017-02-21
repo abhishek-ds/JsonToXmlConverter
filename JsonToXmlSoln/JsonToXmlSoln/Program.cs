d9325ea5ad877f8cacc18686c67114edf0dceb72
Abhishek <abhishekds1992@gmail.com>
2017-02-21 09:18:28 -0500

    Json to XML stream converter

---
+++ JsonToXmlSoln/JsonToXmlSoln/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace JsonToXmlSoln
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            WebClient client = new WebClient();
+
+            //place holder for url and file name
+            //var holder = ConfigurationManager.AppSettings[""];
+
+            Stream stream = client.OpenRead("url");
+            StreamReader streamReader = new StreamReader(stream);
+
+            JObject jObject = JObject.Parse(streamReader.ReadLine());
+            string jsonString = JsonConvert.SerializeObject(jObject);
+
+            XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(jsonString, "Root");
+
+            System.Xml.XmlTextWriter xmlTextWriter = new System.Xml.XmlTextWriter("TestJson.xml", null);
+            xmlTextWriter.Formatting = System.Xml.Formatting.Indented;
+            doc.Save(xmlTextWriter);
+            
+            stream.Close();
+        }
+    }
+}
