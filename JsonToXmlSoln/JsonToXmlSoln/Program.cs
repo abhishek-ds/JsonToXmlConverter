a3f54b6a54c2de741c537b281bf9c28965e502fc
Abhishek <abhishekds1992@gmail.com>
2017-02-21 10:45:39 -0500

    Adding Console Visuals

--- JsonToXmlSoln/JsonToXmlSoln/Program.cs
+++ JsonToXmlSoln/JsonToXmlSoln/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Configuration;
 using System.IO;
 using System.Net;
@@ -13,23 +14,35 @@ namespace JsonToXmlSoln
         {
             WebClient client = new WebClient();
 
+            //DS--Visual Indicators 1
+            Console.WriteLine("Application Started");
+
             //DS--Read Url through App.Config
             string url = ConfigurationManager.AppSettings["TargetUrl"];
             
             //DS--Path and file name to Save as XML from App.config
             string fileName = ConfigurationManager.AppSettings["FileName"];
             Stream stream = client.OpenRead(url);
-
+                        
             //DS--Initializes a new instance of the StreamReader class
             StreamReader streamReader = new StreamReader(stream);
             JObject jObject = JObject.Parse(streamReader.ReadLine());
+            
+            //DS--Visual Indicators
+            Console.WriteLine("JSON Parsing");
 
             //DS--Create a JSON String
-            string jsonString = JsonConvert.SerializeObject(jObject);
+            string jsonString = JsonConvert.SerializeObject(jObject);            
+           
+            //DS--Visual Indicators
+            Console.WriteLine("Xml Conversion Started");
 
             //DS--JSON to XML Conversion
             XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(jsonString, "Root");
-            
+
+            //DS--Visual Indicators
+            Console.WriteLine("Xml is being Saving, might take a while");
+
             //DS--Write to a file in XML format
             System.Xml.XmlTextWriter xmlTextWriter = new System.Xml.XmlTextWriter(fileName, null);
             xmlTextWriter.Formatting = System.Xml.Formatting.Indented;
